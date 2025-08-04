86ff5648a8795137c4751cd8adbcf6e7f1543836
Vedant Acharya <158566818+Ninjacoder-vedant@users.noreply.github.com>
2025-08-04 15:09:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ a = float(input())
 b = float(input())
 print("Enter the operation you want to perform (+,-,/,*)")
 op = input()
-if op=="+" :
+if op=="+":
     print(a+b) 
 elif op=="-":
     print(a-b)
