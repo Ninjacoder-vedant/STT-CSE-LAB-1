217653c38771c72dba005f5a5edd9096ed416c23
Vedant Acharya <vedant.acharya2911@gmail.com>
2025-08-04 15:06:10 +0530

    added code

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
+"""This module performs calculations of 2 operands"""
 for i in range(10):
     print(i)
+print("----Calculator----")
+print("Enter the digits")
+a = float(input())
+b = float(input())
+print("Enter the operation you want to perform (+,-,/,*)")
+op = input()
+if op=="+" :
+    print(a+b) 
+elif op=="-":
+    print(a-b)
+elif op=="/":
+    print(a/b)
+else:
+    print(a*b)
